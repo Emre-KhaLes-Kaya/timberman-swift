dc83b73792c7a5117051b4a1ed0e7af9ed0507f0
Jeremy Novak <jgnovak@gmail.com>
2016-03-07 17:28:30 -0600

    Project organization
    Project organization

---
+++ Project/Chapter 2/Finish/TimberJim/TimberJim/Classes/SpriteName.swift
@@ -0,0 +1,19 @@
+//
+//  SpriteName.swift
+//  TimberJim
+//
+//  Created by Jeremy Novak on 3/7/16.
+//  Copyright © 2016 Jeremy Novak. All rights reserved.
+//
+
+import Foundation
+
+
+class SpriteName {
+    
+    // Environment
+    class var Clouds:String         { return "Clouds" }
+    class var Birds0:String         { return "Birds0" }
+    class var Birds1:String         { return "Birds1" }
+    class var Forest:String         { return "Forest" }
+}
